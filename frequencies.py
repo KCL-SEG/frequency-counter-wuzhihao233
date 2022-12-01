3839f8279ab750ff8d5836a9fa8cf819aa8a13fa
zhihaowu <k21040194@kcl.ac.uk>
2022-12-01 10:00:42 +0000

    frequencies finish

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        frequencies[str(item)] = frequencies.get(str(item), 0) + 1
     return frequencies
